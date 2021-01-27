f081d71c761aa4475057369d62e2048ebfe39475
Sahal Asghar <sahaldos1@gmail.com>
2021-01-27 13:13:45 -0500

    cleared tasks from local storage

--- app.js
+++ app.js
@@ -117,8 +117,30 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
+
+      //remove from local storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+//remove tasks from local storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //clear tasks function
@@ -128,6 +150,14 @@ function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  //clear from local storgage
+  clearTasksFromLocalStorage();
+}
+
+//clear tasks from local storage function
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
 //filter tasks function
